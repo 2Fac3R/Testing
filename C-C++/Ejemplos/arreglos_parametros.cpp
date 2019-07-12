d341f27e14cf93aa86bccaabba572e83bd8b56e7
Gustavo Navarro <genr1818@gmail.com>
2019-07-11 20:31:01 -0500

    Minor changes

--- C-C++/Ejemplos/arreglos_parametros.cpp
+++ C-C++/Ejemplos/arreglos_parametros.cpp
@@ -1,48 +1,45 @@
 #include <iostream>
- 
+
 using namespace std;
- 
-int main(){
- 
+
+int main()
+{
+
     bool esPar(int);
     void mostrarArreglo(int[], int);
- 
+
     int pares[20] = {NULL};
     int nones[20] = {NULL};
-    int i, j=0 , k=0;
- 
-    for(i = 5; i<=100; i += 5){
-        if(esPar(i) == true)
+    int i, j = 0, k = 0;
+
+    for (i = 5; i <= 100; i += 5)
+    {
+        if (esPar(i))
         {
             pares[j++] = i;
- 
         }
         else
         {
             nones[k++] = i;
         }
     }
- 
+
     cout << "Arreglo de numeros pares: ";
     mostrarArreglo(pares, j);
     cout << endl;
     cout << "Arreglo de numeros impar: ";
     mostrarArreglo(nones, k);
- 
- 
+
     return 0;
 }
- 
-bool esPar(int x){
-    if(x%2 == 0){
-        return true;
-    }
-    else{
-        return false;
-    }
+
+bool esPar(int x)
+{
+    return (x % 2 == 0) ? true : false;
 }
- 
-void mostrarArreglo(int arreglo[], int tam){
-    for (int i = 0 ; i < tam ; i++)
+
+void mostrarArreglo(int arreglo[], int tam)
+{
+    for (int i = 0; i < tam; i++)
         cout << arreglo[i] << " ";
 }
--- Python/Switch manual.py
+++ Python/Switch manual.py
@@ -3,16 +3,20 @@
 
 import urllib
 
+
 def hexadecimal(texto):
     return texto.encode("hex")
+
 
 def quote(texto):
     return urllib.quote(texto)
 
+
 def switch(opcion):
     return {
-            "bin2hex" : "hexadecimal",
-            "encode": "quote",
+        "bin2hex": "hexadecimal",
+        "encode": "quote",
     }[opcion]
+
 
 print locals()[switch("encode")]("http://underc0de.org/foro/python/")
