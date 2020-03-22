6a3cf731a2617b187b110e7f2ce4a131a2bab619
2Fac3R <rockeg18@gmail.com>
2020-03-22 15:37:20 -0600

    Learning Go Basics

---
+++ Go/switch.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+	The break statement that is needed at the end of each case
+	is provided automatically in Go
+*/
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+}
--- Python/tutorial/file_read.py
+++ Python/tutorial/file_read.py
@@ -1,2 +1,2 @@
-f = open("test.txt", "r")
-print(f.read())
+filename = open("test.txt", "r")
+print(filename.read())
