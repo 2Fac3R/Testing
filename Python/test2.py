649e483b856833c92a7a550a3d13e3d0eabaed9a
Gustavo Navarro <genr1818@gmail.com>
2022-12-01 16:30:07 -0600

    update

--- Python/test2.py
+++
@@ -1,6 +0,0 @@
-from difflib import SequenceMatcher
-from fuzzywuzzy import fuzz
-
-m = SequenceMatcher(None, "IBM", "IBM Corporation")
-
-print(fuzz.partial_ratio("Rallye Motors Mercedes- Benz", "Rallye Motors | Mercedes-Benz"))
--- Python/test3.py
+++
@@ -1,7 +0,0 @@
-def my_func(x):
-    respuesta = 0
-    for i in range(2000):
-        respuesta += 1
-    return respuesta
-
-print(my_func(0))
